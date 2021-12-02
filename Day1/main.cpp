195e0e23c21f03eb51dcff60d1fa0b22ae82eb92
PiotrKuleta <56878876+PiotrKuleta@users.noreply.github.com>
2021-12-02 10:20:18 +0100

    Update main.cpp

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -4,30 +4,55 @@
 
 int main() {
 
-	std::fstream new_file;
+		std::fstream new_file;
 
 	new_file.open("input", std::ios::in);
 
 	if (new_file.is_open()) {
 
+		std::vector<int> vec;
+
 		int counter = 0;
+
 		int prev_val = 0;
 		int grater_than_prev_counter = 0;
 
+		int prev_slider_sum = 0;
+		int grater_than_prev_sliding_counter = 0;
+
 		std::string line;
 		while (std::getline(new_file, line)) {
-			
-			int current_val = std::stoi(line);
 
-			if (prev_val < current_val && counter!=0) {
-				grater_than_prev_counter++;
+			vec.push_back(std::stoi(line));
+			
+			{ //Check previous value
+				if (counter != 0) {
+					if (vec[counter - 1] < vec[counter]) {
+						grater_than_prev_counter++;
+					}
+				}
+
+				prev_val = vec[counter];
 			}
 
-			prev_val = current_val;
+			{ //Slider 
+				int current_sum = 0;
+				if (counter >= 2) {
+					current_sum = vec[counter] + vec[counter - 1] + vec[counter - 2];
+				}
+
+				if (prev_slider_sum < current_sum && counter >= 3) {
+					grater_than_prev_sliding_counter++;
+				}
+
+				prev_slider_sum = current_sum;
+			}
+
 			counter++;
 		}
 
-		std::cout << "Increased: " << grater_than_prev_counter << " times !!!";
+		std::cout << "In comparison to previous value: : " << grater_than_prev_counter << " times increased !!!" << std::endl <<
+			     "In comparison to previous sliding window: : " << grater_than_prev_sliding_counter << " times increased !!!" << std::endl;
 	}
 
 	return 0;
